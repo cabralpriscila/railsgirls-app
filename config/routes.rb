4d1fb663cccbf0bb9fbd8442f16fac26665ffb20
cabralpriscila <priscilacabral.dev@gmail.com>
2023-07-03 21:43:50 -0700

    feat: :heavy_plus_sign: redirect root to ideas page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :ideas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  root "ideas#index"
 end
